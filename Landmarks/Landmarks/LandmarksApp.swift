8924d757fae7b0e36e7e4119ab093ae9f4be34d4
pjy7814 <pjy7814@naver.com>
2022-11-06 10:06:25 +0900

    complete 1/7

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 박지영 on 2022/11/06.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
