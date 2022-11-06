0f710b3bd2190fe322477f4ca0190ae0422d72a2
pjy7814 <pjy7814@naver.com>
2022-11-06 11:51:27 +0900

    complete 2/7

---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,30 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by 박지영 on 2022/11/06.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .previewDevice(PreviewDevice(rawValue: "iPhone SE(2nd generation)"))
+    }
+}
